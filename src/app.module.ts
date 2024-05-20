99753a7b59fe3de1cc829a1166ae831e36fa8d37
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-19 22:35:17 -0400

    Update: Docker volume names and npm dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      migrationsRun: true,
+      keepConnectionAlive: true,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.DB_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.DB_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
