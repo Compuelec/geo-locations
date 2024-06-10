efa6d05ad45f8be135bd1fc0b149d2304777b328
Carlos Abarca Vega <soporte@compuelec.cl>
2024-06-10 00:31:38 -0400

    feat: Add news module and related entities, controllers, services, and DTOs

---
+++ src/modules/news/entities/news.entity.ts
@@ -0,0 +1,73 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class News {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  article_id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  link: string;
+
+  @Column('simple-array')
+  keywords: string[];
+
+  @Column('simple-array')
+  creator: string[];
+
+  @Column({ nullable: true })
+  video_url: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  pubDate: Date;
+
+  @Column()
+  image_url: string;
+
+  @Column()
+  source_id: string;
+
+  @Column()
+  source_priority: number;
+
+  @Column()
+  source_url: string;
+
+  @Column()
+  source_icon: string;
+
+  @Column()
+  language: string;
+
+  @Column('simple-array')
+  country: string[];
+
+  @Column('simple-array')
+  category: string[];
+
+  @Column({ nullable: true })
+  ai_tag: string;
+
+  @Column({ nullable: true })
+  sentiment: string;
+
+  @Column({ nullable: true })
+  sentiment_stats: string;
+
+  @Column({ nullable: true })
+  ai_region: string;
+
+  @Column({ nullable: true })
+  ai_org: string;
+}
---
+++ src/modules/news/news.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { News } from './entities/news.entity';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class NewsService {
+  constructor(
+    @InjectRepository(News)
+    private newsRepository: Repository<News>,
+    private httpService: HttpService,
+  ) {}
+
+  async fetchAndSaveNews(): Promise<void> {
+    const response = await lastValueFrom(
+      this.httpService.get(
+        `https://newsdata.io/api/1/news?apikey=${process.env.TOKEN_NEWS_API}&q=chile&country=cl&language=es&category=education,other,politics,sports,top`,
+      ),
+    );
+    const newsData = response.data.results;
+
+    for (const article of newsData) {
+      await this.create({
+        ...article,
+        keywords: article.keywords || '',
+        creator: article.creator || '',
+      });
+    }
+  }
+
+  async create(newsData: Partial<News>): Promise<News> {
+    const news = this.newsRepository.create(newsData);
+    if (newsData.article_id) {
+      const existingNews = await this.newsRepository.findOne({
+        where: {
+          article_id: newsData.article_id,
+        },
+      });
+      if (existingNews) {
+        return existingNews;
+      }
+    }
+    return this.newsRepository.save(news);
+  }
+
+  async findAll(): Promise<News[]> {
+    return this.newsRepository.find();
+  }
+
+  async findByDate(date: string): Promise<News[]> {
+    const startDate =
+      new Date(date).getFullYear() +
+      '-' +
+      (new Date(date).getMonth() + 1) +
+      '-' +
+      new Date(date).getUTCDate() +
+      ' 00:00:00';
+    const endDate =
+      new Date(date).getFullYear() +
+      '-' +
+      (new Date(date).getMonth() + 1) +
+      '-' +
+      new Date(date).getUTCDate() +
+      ' 23:59:59';
+
+    const queryBuilder = this.newsRepository.createQueryBuilder('news');
+
+    return queryBuilder
+      .where('news.pubDate >= :startOfDay', { startOfDay: startDate })
+      .andWhere('news.pubDate <= :endOfDay', { endOfDay: endDate })
+      .getMany();
+  }
+}
